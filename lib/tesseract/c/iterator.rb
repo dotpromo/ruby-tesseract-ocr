34ab67fdbdee808b1411327d7504780e348ab2ab
Alexander Simonov <alex@simonov.me>
2015-12-03 12:12:28 +0200

    Add tesseract 3.4 support

--- lib/tesseract/c/iterator.rb
+++ lib/tesseract/c/iterator.rb
@@ -25,258 +25,258 @@
 module Tesseract; module C
 
 module Iterator
-	extend FFI::Inline
+  extend FFI::Inline
 
-	inline 'C++' do |cpp|
-		cpp.include   'tesseract/resultiterator.h'
-		cpp.libraries 'tesseract'
+  inline 'C++' do |cpp|
+    cpp.include   'tesseract/resultiterator.h'
+    cpp.libraries 'tesseract'
 
-		cpp.raw 'using namespace tesseract;'
+    cpp.raw 'using namespace tesseract;'
 
-		cpp.eval {
-			enum :PolyBlockType, [
-				:UNKNOWN,
-				:FLOWING_TEXT, :HEADING_TEXT, :PULLOUT_TEXT, :TABLE, :VERTICAL_TEXT, :CAPTION_TEXT,
-				:FLOWING_IMAGE, :HEADING_IMAGE, :PULLOUT_IMAGE,
-				:HORZ_LINE, :VERT_LINE, :NOISE, :COUNT
-			]
+    cpp.eval {
+      enum :PolyBlockType, [
+        :UNKNOWN,
+        :FLOWING_TEXT, :HEADING_TEXT, :PULLOUT_TEXT, :TABLE, :VERTICAL_TEXT, :CAPTION_TEXT,
+        :FLOWING_IMAGE, :HEADING_IMAGE, :PULLOUT_IMAGE,
+        :HORZ_LINE, :VERT_LINE, :NOISE, :COUNT
+      ]
 
-			enum :PageIteratorLevel, [
-				:BLOCK, :PARAGRAPH, :LINE, :WORD, :SYMBOL
-			]
+      enum :PageIteratorLevel, [
+        :BLOCK, :PARAGRAPH, :LINE, :WORD, :SYMBOL
+      ]
 
-			orientation = enum :UP, :RIGHT, :DOWN, :LEFT
-			direction   = enum :LEFT_TO_RIGHT, :RIGHT_TO_LEFT, :TOP_TO_BOTTOM
+      orientation = enum :UP, :RIGHT, :DOWN, :LEFT
+      direction   = enum :LEFT_TO_RIGHT, :RIGHT_TO_LEFT, :TOP_TO_BOTTOM
 
-			BoundingBox = Class.new(FFI::Struct) {
-				layout \
-					:left,   :int,
-					:top,    :int,
-					:right,  :int,
-					:bottom, :int
-			}
+      BoundingBox = Class.new(FFI::Struct) {
+        layout \
+          :left,   :int,
+          :top,    :int,
+          :right,  :int,
+          :bottom, :int
+      }
 
-			Image = Class.new(FFI::Struct) {
-				layout \
-					:pix,  :pointer,
-					:x,    :int,
-					:y,    :int
-			}
+      Image = Class.new(FFI::Struct) {
+        layout \
+          :pix,  :pointer,
+          :x,    :int,
+          :y,    :int
+      }
 
-			Baseline = Class.new(FFI::Struct) {
-				layout \
-					:x1, :int,
-					:y1, :int,
-					:x2, :int,
-					:y2, :int
-			}
+      Baseline = Class.new(FFI::Struct) {
+        layout \
+          :x1, :int,
+          :y1, :int,
+          :x2, :int,
+          :y2, :int
+      }
 
-			Orientation = Class.new(FFI::Struct) {
-				layout \
-					:orientation,       orientation,
-					:writing_direction, direction,
-					:textline_order,    direction,
-					:deskew_angle,      :float
-			}
+      Orientation = Class.new(FFI::Struct) {
+        layout \
+          :orientation,       orientation,
+          :writing_direction, direction,
+          :textline_order,    direction,
+          :deskew_angle,      :float
+      }
 
-			FontAttributes = Class.new(FFI::Struct) {
-				layout \
-				:id,        :int,
-				:name,      :string,
-				:pointsize, :int,
+      FontAttributes = Class.new(FFI::Struct) {
+        layout \
+        :id,        :int,
+        :name,      :string,
+        :pointsize, :int,
 
-				:is_bold,       :bool,
-				:is_italic,     :bool,
-				:is_underlined, :bool,
-				:is_monospace,  :bool,
-				:is_serif,      :bool,
-				:is_smallcaps,  :bool
-			}
+        :is_bold,       :bool,
+        :is_italic,     :bool,
+        :is_underlined, :bool,
+        :is_monospace,  :bool,
+        :is_serif,      :bool,
+        :is_smallcaps,  :bool
+      }
 
-			typedef BoundingBox.by_value,    :BoundingBox
-			typedef Image.by_value,          :Image
-			typedef Baseline.by_value,       :Baseline
-			typedef Orientation.by_value,    :OrientationResult
-			typedef FontAttributes.by_value, :FontAttributes
-		}
+      typedef BoundingBox.by_value,    :BoundingBox
+      typedef Image.by_value,          :Image
+      typedef Baseline.by_value,       :Baseline
+      typedef Orientation.by_value,    :OrientationResult
+      typedef FontAttributes.by_value, :FontAttributes
+    }
 
-		cpp.raw %{
-			typedef struct BoundingBox {
-				int left;
-				int top;
-				int right;
-				int bottom;
-			} BoundingBox;
+    cpp.raw %{
+      typedef struct BoundingBox {
+        int left;
+        int top;
+        int right;
+        int bottom;
+      } BoundingBox;
 
-			typedef struct Image {
-				Pix* pix;
-				int  x;
-				int  y;
-			} Image;
+      typedef struct Image {
+        Pix* pix;
+        int  x;
+        int  y;
+      } Image;
 
-			typedef struct Baseline {
-				int x1;
-				int y1;
-				int x2;
-				int y2;
-			} Baseline;
+      typedef struct Baseline {
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+      } Baseline;
 
-			typedef struct OrientationResult {
-				Orientation      orientation;
-				WritingDirection writing_direction;
-				TextlineOrder    textline_order;
-				float            deskew_angle;
-			} OrientationResult;
+      typedef struct OrientationResult {
+        Orientation      orientation;
+        WritingDirection writing_direction;
+        TextlineOrder    textline_order;
+        float            deskew_angle;
+      } OrientationResult;
 
-			typedef struct FontAttributes {
-				int         id;
-				const char* name;
-				int         pointsize;
+      typedef struct FontAttributes {
+        int         id;
+        const char* name;
+        int         pointsize;
 
-				bool is_bold;
-				bool is_italic;
-				bool is_underlined;
-				bool is_monospace;
-				bool is_serif;
-				bool is_smallcaps;
-			} FontAttributes;
-		}
+        bool is_bold;
+        bool is_italic;
+        bool is_underlined;
+        bool is_monospace;
+        bool is_serif;
+        bool is_smallcaps;
+      } FontAttributes;
+    }
 
-		cpp.function %{
-			void destroy (PageIterator* it) {
-				delete it;
-			}
-		}
+    cpp.function %{
+      void destroy (PageIterator* it) {
+        delete it;
+      }
+    }
 
-		cpp.function %{
-			void begin (PageIterator* it) {
-				it->Begin();
-			}
-		}
+    cpp.function %{
+      void begin (PageIterator* it) {
+        it->Begin();
+      }
+    }
 
-		cpp.function %{
-			bool next (PageIterator* it, PageIteratorLevel level) {
-				return it->Next(level);
-			}
-		}
+    cpp.function %{
+      bool next (PageIterator* it, PageIteratorLevel level) {
+        return it->Next(level);
+      }
+    }
 
-		cpp.function %{
-			bool is_at_beginning_of (PageIterator* it, PageIteratorLevel level) {
-				return it->IsAtBeginningOf(level);
-			}
-		}
+    cpp.function %{
+      bool is_at_beginning_of (PageIterator* it, PageIteratorLevel level) {
+        return it->IsAtBeginningOf(level);
+      }
+    }
 
-		cpp.function %{
-			bool is_at_final_element (PageIterator* it, PageIteratorLevel level, PageIteratorLevel element) {
-				return it->IsAtFinalElement(level, element);
-			}
-		}
+    cpp.function %{
+      bool is_at_final_element (PageIterator* it, PageIteratorLevel level, PageIteratorLevel element) {
+        return it->IsAtFinalElement(level, element);
+      }
+    }
 
-		cpp.function %{
-			BoundingBox bounding_box (PageIterator* it, PageIteratorLevel level) {
-				BoundingBox result;
+    cpp.function %{
+      BoundingBox bounding_box (PageIterator* it, PageIteratorLevel level) {
+        BoundingBox result;
 
-				it->BoundingBox(level, &result.left, &result.top, &result.right, &result.bottom);
+        it->BoundingBox(level, &result.left, &result.top, &result.right, &result.bottom);
 
-				return result;
-			}
-		}
+        return result;
+      }
+    }
 
-		cpp.function %{
-			PolyBlockType block_type (PageIterator* it) {
-				return it->BlockType();
-			}
-		}
+    cpp.function %{
+      PolyBlockType block_type (PageIterator* it) {
+        return it->BlockType();
+      }
+    }
 
-		cpp.function %{
-			Pix* get_binary_image (PageIterator* it, PageIteratorLevel level) {
-				return it->GetBinaryImage(level);
-			}
-		}
+    cpp.function %{
+      Pix* get_binary_image (PageIterator* it, PageIteratorLevel level) {
+        return it->GetBinaryImage(level);
+      }
+    }
 
-		cpp.function %{
-			Image get_image (PageIterator* it, PageIteratorLevel level, int padding) {
-				Image result;
+    cpp.function %{
+      Image get_image (PageIterator* it, PageIteratorLevel level, int padding) {
+        Image result;
 
-				result.pix = it->GetImage(level, padding, &result.x, &result.y);
+        result.pix = it->GetImage(level, padding, result.pix, &result.x, &result.y);
 
-				return result;
-			}
-		}
+        return result;
+      }
+    }
 
-		cpp.function %{
-			Baseline baseline (PageIterator* it, PageIteratorLevel level) {
-				Baseline result;
+    cpp.function %{
+      Baseline baseline (PageIterator* it, PageIteratorLevel level) {
+        Baseline result;
 
-				it->Baseline(level, &result.x1, &result.y1, &result.x2, &result.y2);
+        it->Baseline(level, &result.x1, &result.y1, &result.x2, &result.y2);
 
-				return result;
-			}
-		}
+        return result;
+      }
+    }
 
-		cpp.function %{
-			OrientationResult orientation (PageIterator* it) {
-				OrientationResult result;
+    cpp.function %{
+      OrientationResult orientation (PageIterator* it) {
+        OrientationResult result;
 
-				it->Orientation(&result.orientation, &result.writing_direction, &result.textline_order, &result.deskew_angle);
+        it->Orientation(&result.orientation, &result.writing_direction, &result.textline_order, &result.deskew_angle);
 
-				return result;
-			}
-		}
+        return result;
+      }
+    }
 
-		cpp.function %{
-			char* get_utf8_text (ResultIterator* it, PageIteratorLevel level) {
-				return it->GetUTF8Text(level);
-			}
-		}, blocking: true
+    cpp.function %{
+      char* get_utf8_text (ResultIterator* it, PageIteratorLevel level) {
+        return it->GetUTF8Text(level);
+      }
+    }, blocking: true
 
-		cpp.function %{
-			float confidence (ResultIterator* it, PageIteratorLevel level) {
-				return it->Confidence(level);
-			}
-		}, blocking: true
+    cpp.function %{
+      float confidence (ResultIterator* it, PageIteratorLevel level) {
+        return it->Confidence(level);
+      }
+    }, blocking: true
 
-		cpp.function %{
-			FontAttributes word_font_attributes (ResultIterator* it) {
-				FontAttributes result;
+    cpp.function %{
+      FontAttributes word_font_attributes (ResultIterator* it) {
+        FontAttributes result;
 
-				result.name = it->WordFontAttributes(&result.is_bold, &result.is_italic, &result.is_underlined,
-					&result.is_monospace, &result.is_serif, &result.is_smallcaps, &result.pointsize, &result.id);
+        result.name = it->WordFontAttributes(&result.is_bold, &result.is_italic, &result.is_underlined,
+          &result.is_monospace, &result.is_serif, &result.is_smallcaps, &result.pointsize, &result.id);
 
-				return result;
-			}
-		}
+        return result;
+      }
+    }
 
-		cpp.function %{
-			bool word_is_from_dictionary (ResultIterator* it) {
-				return it->WordIsFromDictionary();
-			}
-		}
+    cpp.function %{
+      bool word_is_from_dictionary (ResultIterator* it) {
+        return it->WordIsFromDictionary();
+      }
+    }
 
-		cpp.function %{
-			bool word_is_numeric (ResultIterator* it) {
-				return it->WordIsNumeric();
-			}
-		}
+    cpp.function %{
+      bool word_is_numeric (ResultIterator* it) {
+        return it->WordIsNumeric();
+      }
+    }
 
-		cpp.function %{
-			bool symbol_is_superscript (ResultIterator* it) {
-				return it->SymbolIsSuperscript();
-			}
-		}
+    cpp.function %{
+      bool symbol_is_superscript (ResultIterator* it) {
+        return it->SymbolIsSuperscript();
+      }
+    }
 
-		cpp.function %{
-			bool symbol_is_subscript (ResultIterator* it) {
-				return it->SymbolIsSubscript();
-			}
-		}
+    cpp.function %{
+      bool symbol_is_subscript (ResultIterator* it) {
+        return it->SymbolIsSubscript();
+      }
+    }
 
-		cpp.function %{
-			bool symbol_is_dropcap (ResultIterator* it) {
-				return it->SymbolIsDropcap();
-			}
-		}
-	end
+    cpp.function %{
+      bool symbol_is_dropcap (ResultIterator* it) {
+        return it->SymbolIsDropcap();
+      }
+    }
+  end
 end
 
 end; end
